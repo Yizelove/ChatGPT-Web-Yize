0c68fe9cbc0ed6c30355d4abf9a9d50488d466f1
绎泽 <108044328+Yizelove@users.noreply.github.com>
2023-04-27 12:45:21 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,10 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "ChatGPT - Yize",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "ChatGPT - Yize",
     statusBarStyle: "default",
   },
   themeColor: "#fafafa",
