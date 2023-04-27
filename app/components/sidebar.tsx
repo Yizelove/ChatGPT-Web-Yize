169ced140f88559b39a8d6fc1e4f5505ba473e79
绎泽 <108044328+Yizelove@users.noreply.github.com>
2023-04-27 13:02:33 +0800

    Update sidebar.tsx
    更改首页标题和描述

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -90,9 +90,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>ChatGPT-Yize</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          你最棒的AI助手！
         </div>
         <div className={styles["sidebar-logo"]}>
           <ChatGptIcon />
