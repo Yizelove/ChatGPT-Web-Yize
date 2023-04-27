c77b0ee440a3b6c608e2cae2babc160204886ec0
绎泽 <108044328+Yizelove@users.noreply.github.com>
2023-04-27 20:03:26 +0800

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -52,7 +52,7 @@ fi
 
 # Clone the repository and install dependencies
 git clone https://github.com/Yizelove/ChatGPT-Web-Yize
-cd ChatGPT-Next-Web
+cd ChatGPT-Web-Yize
 yarn install
 
 # Prompt user for environment variables
